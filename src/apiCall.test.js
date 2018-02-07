bb43d9b7d6fa53f34baa6a6a784ee2882df97473
Michelle Hoffman <hoffman.michelle.e@gmail.com>
2018-02-06 18:55:40 -0700

    Set up tests for all fetch functions:

---
+++ src/apiCall.test.js
@@ -0,0 +1,40 @@
+import mockData from './mockData';
+import apiKey from './apiKey';
+import { getMovies } from './apiCall';
+
+/* eslint-disable */
+
+describe('fetch movie data', () => {
+  beforeEach(() => {
+    const mockMovies = mockData;
+    window.fetch = jest.fn().mockImplementation(() => {
+      return Promise.resolve({
+        status: 200,
+        json: () => Promise.resolve(mockMovies)
+      })
+    })
+  });
+
+  it('should fetch with the correct params', () => {
+    const expected = `https://api.themoviedb.org/3/discover/movie?api_key=${apiKey}&primary_release_date.gte=2018-02-06&primary_release_date.lte=2018-03-31/`;
+    
+    getMovies();
+
+    expect(window.fetch).toHaveBeenCalledWith(expected);
+  });
+
+  it('should return a movie array', async () => {
+    expect(await getMovies()).toEqual(mockMovies);
+  });
+
+  it('should return an error message if fetch fails', async () => {
+    window.fetch = jest.fn().mockImplementation(() => {
+      return Promise.reject({
+        status: 500,
+        json: () => Promise.resolve('Error fetching movies')
+      })
+    })
+
+    expect(getMovies()).toEqual('Error fetching movies');
+  });
+})
---
+++ src/mockData.js
@@ -0,0 +1,16 @@
+export const mockData = [{
+  "vote_count": 20,
+  "id": 337167,
+  "video": false,
+  "vote_average": 6.4,
+  "title": "Fifty Shades Freed",
+  "popularity": 45.670468,
+  "poster_path": "/iUDFe6B6bFPOAE13uoqH4eEU1cF.jpg",
+  "original_language": "en",
+  "original_title": "Fifty Shades Freed",
+  "genre_ids": [18, 10749],
+  "backdrop_path": "/iuyE1qy9HoTsMg0DHB1KJxM94DJ.jpg",
+  "adult": false,
+  "overview": "The third installment of the Fifty Shades Trilogy.",
+  "release_date": "2018-02-07"
+}];
