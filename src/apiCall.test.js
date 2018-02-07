fd4867a4901a6625931973cc3d270bdc235358d6
Michelle Hoffman <hoffman.michelle.e@gmail.com>
2018-02-06 19:02:55 -0700

    Set up tests for postgres fetch

--- src/apiCall.test.js
+++ src/apiCall.test.js
@@ -38,3 +38,38 @@ describe('fetch movie data', () => {
     expect(getMovies()).toEqual('Error fetching movies');
   });
 })
+
+describe('fetch favorites data', () => {
+  beforeEach(() => {
+    const mockMovies = mockData;
+    window.fetch = jest.fn().mockImplementation(() => {
+      return Promise.resolve({
+        status: 200,
+        json: () => Promise.resolve(mockMovies)
+      })
+    })
+  });
+
+  it('should fetch with the correct params', () => {
+    const expected = `https://api.themoviedb.org/3/discover/movie?api_key=${apiKey}&primary_release_date.gte=2018-02-06&primary_release_date.lte=2018-03-31/`;
+    
+    getFavorites();
+
+    expect(window.fetch).toHaveBeenCalledWith(expected);
+  });
+
+  it('should return a movie array', async () => {
+    expect(await getFavorites()).toEqual(mockMovies);
+  });
+
+  it('should return an error message if fetch fails', async () => {
+    window.fetch = jest.fn().mockImplementation(() => {
+      return Promise.reject({
+        status: 500,
+        json: () => Promise.resolve('Error fetching movies')
+      })
+    })
+
+    expect(getFavorites()).toEqual('Error fetching movies');
+  });
+})
