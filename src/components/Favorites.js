1bf98e36607e3eb086597a30fb1ab3c8a8690ba0
Melissa Wagasky <melissawagasky@gmail.com>
2018-02-07 10:34:03 -0700

    Add test code and new files

---
+++ src/components/Favorites.js
@@ -0,0 +1,12 @@
+import React from 'react';
+// some psuedo code for test
+
+const Favorites = () => {
+  return (
+    <div>
+      <p>I'm the favorites!</p>
+    </div>
+  )
+}
+
+export default Favorites;
---
+++ src/components/Splash.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Controls from './Controls'
+
+const Splash = () => {
+  return (
+    <div>
+      <p>I'm the splash page!</p>
+      <Controls />
+    </div>
+  )
+}
+
+export default Splash;
