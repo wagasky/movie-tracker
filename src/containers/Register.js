fe39e1cee2262aad060db3d52bc3c68d3e6b9b25
Melissa Wagasky <melissawagasky@gmail.com>
2018-02-10 09:50:03 -0700

    Add automatic user login upon registration

--- src/containers/Register.js
+++ src/containers/Register.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { setUser} from '../actions/index';
+import { setUser } from '../actions/index';
 import { userSignIn, addNewUser } from '../apiCall'
 
 
@@ -32,10 +32,24 @@ class Register extends Component {
     const results = await addNewUser({ name, email, password });
     const userResults = await userSignIn(email, password);
 
+    const user = await userResults.data
+
+    this.autoLogIn(user)
+
+
     // save for later - set current_user as recently registered user 
     // to save user a step
     // const user = await userResults.data
     // this.props.setUser(user)
+  }
+
+  autoLogIn = async (user) => {
+    const { email, password } = user
+
+    const results = await userSignIn(email, password);
+    const userData = await results.data
+    this.props.setUser(userData)
+
   }
 
   render() {
@@ -52,8 +66,10 @@ class Register extends Component {
   }
 }
 
+
 export const mapDispatchToProps = (dispatch) => ({
   addNewUser: (user) => dispatch(addNewUser(user)),
+  setUser: (user) => dispatch(setUser(user))
 })
 
 export default connect(null, mapDispatchToProps)(Register)
