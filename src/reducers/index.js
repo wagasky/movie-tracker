d99c398f7862390c89c0f23adf3809a22e4116dc
Melissa Wagasky <melissawagasky@gmail.com>
2018-02-09 23:56:35 -0700

    Begin implementing API call to store a new user

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,6 +2,7 @@ import { combineReducers } from 'redux';
 import { moviesReducer } from './moviesReducer';
 import { favoritesReducer } from './favoritesReducer';
 import { loginsReducer } from './loginsReducer';
+import { registerReducer } from './registerReducer'
 
 export const rootReducer = combineReducers({
   movies: moviesReducer,
---
+++ src/reducers/registerReducer.js
@@ -0,0 +1,6 @@
+export const registerReducer = (state={}, action) => {
+  switch(action.type) {
+    default: 
+      return state
+  }
+}
