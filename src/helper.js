8875c537acb5afffc5cdba28017a0707c35ac1d8
Michelle Hoffman <hoffman.michelle.e@gmail.com>
2018-02-10 19:56:58 -0700

    Allow user to delete favorites from All movies page

--- src/helper.js
+++ src/helper.js
@@ -1,5 +1,5 @@
 export const cleanedResults = (results) => {
-  const movieData = results.map( movie => Object.assign(movie, {favorite: false}));
+  const movieData = results.map( movie => Object.assign(movie, { favorite: false }));
 
   return movieData;
 };
