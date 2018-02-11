60b12abb41bfb07b5b6d0a85c3afc9be9810df9e
Natalie Montoya <nataliesbarron@gmail.com>
2018-02-11 09:06:02 -0700

    Notifications at registration
    alert user that account exists if they try to create a new account with an existing email address
    
    alert user that their account has been created

--- src/containers/Register.js
+++ src/containers/Register.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { redirect, withRouter } from 'react-router-dom';
+import { redirect, withRouter, Link } from 'react-router-dom';
 import { setUser } from '../actions/index';
-import { userSignIn, addNewUser } from '../apiCall'
+import { userSignIn, addNewUser } from '../apiCall';
 
 class Register extends Component {
   constructor(props) {
@@ -12,7 +12,8 @@ class Register extends Component {
       name: '',
       email: '',
       password: '',
-      errorMessage: null
+      errorMessage: null,
+      newAcctMessage: null
     }
   }
 
@@ -26,25 +27,19 @@ class Register extends Component {
 
   registerSubmit = async (e) => {
     e.preventDefault();
-
     const { name, email, password } = this.state
     const results = await addNewUser({ name, email, password });
     const userResults = await userSignIn(email, password);
-    const user = await userResults.data;
-    this.autoLogIn(user)
+    const user = await userResults.data; 
+
+    this.updateMessage(results)
   }
 
-  autoLogIn = async (user) => {
-    const { email, password } = this.state;
-
-    if (!user) {
-      alert('That email is already registered. You can login instead');
-      // this.setState({errorMessage: "That email is already registered. Please try again."})
-      this.props.history.push('/login');
+  updateMessage = async (results) => {
+    if(results.status !== 'success') {
+      this.setState({errorMessage: "That email is already registered. Please try again or login."})
     } else {
-      const results = await userSignIn(email, password);
-      const userData = await results.data;
-      this.props.setUser(userData)
+      this.setState({newAcctMessage: "Registration successful. Please login."})
     }
   }
 
@@ -52,12 +47,14 @@ class Register extends Component {
     return (
       <div className="log-in-form">
         <form onSubmit={ this.registerSubmit }>
+          <h3 className="acct-create">{ this.state.newAcctMessage }</h3>
           <input type="text" className="name" placeholder="Your Name" name="email" onChange={this.handleChange}/>
           <input type="text" className="email" placeholder="email address" name="email" onChange={this.handleChange}/>
           <input type="password" className="password" placeholder="password" name="password" onChange={this.handleChange}/>
           <button type="submit"
                   className="submit-button">Register</button>
           <h3 className="login-error">{ this.state.errorMessage }</h3>
+          <Link to="/login">Login</Link>
         </form>
       </div>
     )
